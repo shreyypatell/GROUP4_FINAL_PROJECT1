b0a82af495fb5cc31716c29ddfcae2b299c0c686
shreyypatell <theshreypatel04@gmail.com>
2024-07-25 16:13:31 -0500

    Add Selenium tests setup for MyAnimeList

---
+++ FinalProject/MyAnimeListTests.cs
@@ -0,0 +1,51 @@
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyAnimeListTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+        private EmailService emailService;
+        private String testEmail,
+        testUserName = "test63858142", testPassword = "#Test@123";
+        string downloadPath = "C:\\Users\\91886\\Downloads\\";
+
+
+        [SetUp]
+        public async Task Setup()
+        {
+
+            // Change user agent to mimic a real browser
+
+            driver = new FirefoxDriver();
+
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(100));
+
+            emailService = new EmailService();
+
+            testEmail = await emailService.RegisterAndGetEmailAsync();
+
+
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            driver.Quit();
+        }
+    }
+}
